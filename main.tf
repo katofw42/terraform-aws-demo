c4c2e6cda9b658c428eac7611090abc876f0ddc1
Tomochika Kato <139940984+katofw42@users.noreply.github.com>
2025-09-19 00:28:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# サブネット
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
 data "aws_ssm_parameter" "amazon_linux_2023_ami" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
@@ -9,9 +20,5 @@ data "aws_ssm_parameter" "amazon_linux_2023_ami" {
 resource "aws_instance" "amazon_linux_2023" {
   ami           = data.aws_ssm_parameter.amazon_linux_2023_ami.value
   instance_type = "t3.small"
-
-  tags = {
-    Name = "Turtle-Terraform"
-  }
-
+  subnet_id     = aws_subnet.main.id
 }
