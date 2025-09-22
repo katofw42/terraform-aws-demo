4f049d21558a8469bacd7e4496dc4de8daa5fdda
Tomochika Kato <139940984+katofw42@users.noreply.github.com>
2025-09-22 10:31:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ssm_parameter" "amazon_linux_2023_ami" {
 
 resource "aws_instance" "amazon_linux_2023" {
   ami           = data.aws_ssm_parameter.amazon_linux_2023_ami.value
-  instance_type = "t2.small"
+  instance_type = "m5.xlarge"
   subnet_id     = aws_subnet.main.id
 }
 
