e57c6cef60166d8afa9ad576b9b94ccab3024cab
kato <katofw42@gmail.com>
2025-09-30 14:12:03 +0900

    create vpc

--- main.tf
+++ main.tf
@@ -6,21 +6,3 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
-
-# サブネット
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-}
-
-data "aws_ssm_parameter" "amazon_linux_2023_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-}
-
-resource "aws_instance" "amazon_linux_2023" {
-  ami           = data.aws_ssm_parameter.amazon_linux_2023_ami.value
-  instance_type = "t3.large"
-  subnet_id     = aws_subnet.main.id
-}
-
-# test3
