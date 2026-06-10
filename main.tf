a8dada5bf9900468e9ce5dea90ecd8138159769a
Tomochika Kato <139940984+katofw42@users.noreply.github.com>
2026-06-10 13:56:19 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instanc" "web" {
   ami           = "ami-0e68e34976bb4db93"
   instance_type = "t3.micro"
 }
